b11ca480a4cc4e683e88035820ce6e131feaa145
Matthew Whited <matt@whited.us>
2025-07-20 19:26:36 -0400

    test

---
+++ client-builder/SwaggerClientBuilder/SwaggerClientBuilder.Cli/Program.cs
@@ -0,0 +1,38 @@
+﻿using HandlebarsDotNet;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SwaggerClientBuilder.Cli
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var swaggerFile = "swagger.json";
+
+            if (!File.Exists(swaggerFile))
+            {
+                Console.WriteLine("Download swagger.json");
+                using var client = new HttpClient()
+                {
+                    BaseAddress = new Uri("https://localhost:7443")
+                };
+                var response = await client.GetAsync("/swagger/all/swagger.json");
+                var content = await response.Content.ReadAsStringAsync();
+                await File.WriteAllTextAsync(swaggerFile, content);
+            }
+
+            var jsonText = await File.ReadAllTextAsync(swaggerFile);
+            var json = JsonDocument.Parse(jsonText);
+
+            //var template = 
+            //Handlebars.Compile()
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ toys/throwaway/DataToBinary/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DataToBinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Repos\mwwhited\DeviceBridge\";
+            foreach (var file in Directory.EnumerateFiles(path, "*.csv"))
+            {
+                //var file = @"C:\Repos\mwwhited\DeviceBridge\BusDatav2.csv";
+                var lines = File.ReadAllLines(file);
+                var bytes = lines.Skip(6).Select(l => byte.Parse(l.Split(",").Last())).ToArray();
+                var outFile = Path.ChangeExtension(file, ".bin");
+                File.WriteAllBytes(outFile, bytes);
+            }
+        }
+    }
+}
